4776791e11347fe7ee9ac8bed0d057a2233c5586
matteodragotto <matteodrag8@gmail.com>
2025-01-13 16:50:27 +0100

    [feat] footer navigation section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,56 +2,62 @@ const Footer = () => {
   return (
     <footer>
       <section id="navigation">
-        <nav>
-          <ul className="d-flex m-0">
-            <li><a href="#">Digital Comics</a></li>
-            <li><a href="#">DC Merchandise</a></li>
-            <li><a href="#">Subscription</a></li>
-            <li><a href="#">Comic Shop Locator</a></li>
-            <li><a href="#">DC Power Visa</a></li>
-          </ul>
-        </nav>
+        <div className="container h-100 w-100">
+          <nav>
+            <ul className="d-flex m-0 justify-content-between p-5">
+              <li><a href="#">Digital Comics</a></li>
+              <li><a href="#">DC Merchandise</a></li>
+              <li><a href="#">Subscription</a></li>
+              <li><a href="#">Comic Shop Locator</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </nav>
+        </div>
       </section>
       <section id="footer-top">
-        <nav>
-          <ul>
-            <h5>DC Comics</h5>
-            <li><a href="#">Characters</a></li>
-            <li><a href="#">Comics</a></li>
-            <li><a href="#">Movies</a></li>
-            <li><a href="#">Tv</a></li>
-            <li><a href="#">Games</a></li>
-            <li><a href="#">Videos</a></li>
-            <li><a href="#">News</a></li>
-          </ul>
-          <ul>
-            <h5>SHOP</h5>
-            <li><a href="#">Shop DC</a></li>
-            <li><a href="#">Shop DC Collectibles</a></li>
-          </ul>
-          <ul>
-            <h5>DC</h5>
-            <li><a href="#">Terms Of Use</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Ad Choices</a></li>
-            <li><a href="#">Advertising</a></li>
-            <li><a href="#">Jobs</a></li>
-            <li><a href="#">Subscriptions</a></li>
-            <li><a href="#">Talent Workshops</a></li>
-            <li><a href="#">CPSC Certificates</a></li>
-            <li><a href="#">Ratings</a></li>
-            <li><a href="#">Shop Help</a></li>
-            <li><a href="#">Contact Us</a></li>
-          </ul>
-          <ul>
-            <h5>SITES</h5>
-            <li><a href="#">DC</a></li>
-            <li><a href="#">MAD Magazine</a></li>
-            <li><a href="#">DC Kids</a></li>
-            <li><a href="#">DC Universe</a></li>
-            <li><a href="#">DC Power Visa</a></li>
-          </ul>
-        </nav>
+        <div className="container d-flex h-100 p-4">
+          <nav className="d-flex flex-column">
+            <ul>
+              <h5>DC Comics</h5>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">Tv</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+            <ul>
+              <h5>SHOP</h5>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </nav>
+          <nav className="d-flex">
+            <ul>
+              <h5>DC</h5>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Ad Choices</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscriptions</a></li>
+              <li><a href="#">Talent Workshops</a></li>
+              <li><a href="#">CPSC Certificates</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+            <ul>
+              <h5>SITES</h5>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </nav>
+        </div>
       </section>
       <section id="footer-bottom">
         <button></button>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,11 @@
 const Main = () => {
   return (
     <main>
-      <section id="content" className="container d-flex align-items-center">
-        <h2 className="text-light">-- Content goes here--</h2>
+      <section id="content">
+        <div className="container d-flex align-items-center h-100">
+          <h2 className="text-light">-- Content goes here--</h2>
+        </div>
+
       </section>
     </main>
   )
